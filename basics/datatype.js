ca1501865f95b1dc09bf942bfd99d5ee99ab6dc5
rituraj677 <rajritu2107@gmail.com>
2026-02-18 07:24:46 +0000

    backup before shutdown

---
+++ basics/datatype.js
@@ -0,0 +1,20 @@
+"use strict"; 
+
+// alert(3+3) // we are using node js ,not browser
+ let name ="happy"
+ let state=null //it means this is empty
+
+ let age =22
+ let isLoggedIn=false
+
+ // number =>2 to power 53
+ //bigint     
+ //string 
+ // boolean =>true/false
+ //null=>standalone value 
+ //undefined =>
+    //symnol =>
+
+
+
+ 
