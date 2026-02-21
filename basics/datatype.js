aa20e23257e5f9482b117f216554dc2863a4e8f6
rituraj677 <rajritu2107@gmail.com>
2026-02-21 18:10:54 +0000

    backup before shutdown

--- basics/datatype.js
+++ basics/datatype.js
@@ -5,7 +5,7 @@
  let state=null //it means this is empty
 
  let age =22
- let isLoggedIn=false
+//  let isLoggedIn=false
 
  // number =>2 to power 53
  //bigint     
@@ -17,9 +17,38 @@
 
  //object 
 
- console.log(typeof"happy");
- 
+//  console.log(typeof null);//oject
+//  console.log(typeof undefined);//undefined
 
+ let score="33abc";
+//  console.log(typeof score );
+//  console.log(typeof(score))
+ 
+ let valueInNumber=Number (score)
+//   console.log( valueInNumber)
+
+ 
+ //"33">=33//can be converted into number but 
+ //"33abc "=willl not convert into number so it will give nAn  
+ //true=>1;false=>0
+  
+
+ let LoggedIn = 1;
+
+let booleanLoggedIn = Boolean(LoggedIn);
+
+console.log(booleanLoggedIn);
+
+// " " =false
+ let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
+
+
+let value=3
+let negvalue=-value
 
 
  
---
+++ basics/op.js
@@ -0,0 +1,71 @@
+
+//console.log(2+3)
+//console.log(2*3)
+//console.log(2/3)
+//console.log(2+3)
+//console.log(2%3)
+//console.log()
+////console.log(2**3)//used in powers
+
+
+  //**************operations***********//
+let str1="hello"
+let str2="happpy"
+
+// let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+//  console.log(1+"2")
+//  console.log("1"+2+2)
+//  console.log(1+2+"3")
+ // when string comes first then after it every elemnt is considerd as string and when it comes last then firslty before work done then string will come 
+
+// console.log(true)
+// console.log(+true)
+
+// let num1,num2,num3
+
+// num1=num2=num3=2+2//this is very bad in przctice
+
+// let gamecounter =100;
+// gamecounter++;
+// console.log(gamecounter)
+
+//****conditional operators ********
+// /
+
+console.log("2">1)
+console.log("02">1)
+
+console.log(null>0)
+console.log(null==0)
+
+console.log(null>=0)
+
+// an equality check == and comparisons <>=<= work diffrently
+//comparisons convert null to a number treating it as 0 
+//that's why null>=0 is true and null>0 is false 
+
+
+//===(strict check)
+
+console.log("2"===2)//false datatype ia diff
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
