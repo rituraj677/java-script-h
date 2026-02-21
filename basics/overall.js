16d6f782f54fdf1e6a3b154ff377597f80a29a64
rituraj677 <rajritu2107@gmail.com>
2026-02-21 18:46:52 +0000

    backup before shutdown

---
+++ basics/overall.js
@@ -0,0 +1,37 @@
+//primitive
+
+// 7 types :strings,Number,Boolean,null,undefined,symbol,BigInt
+
+//  const bignumber=23628n
+//  const num=5;
+//  const booleanloggedin=Boolean (true);
+//   const outsidetemp=null;
+//   let useremail;
+
+//   const id =symbol('123')
+ const anotherId=symbol('123')
+
+ // console.log(id==anotherId)
+
+
+
+//console.log(booleanloggedin)
+//Reference type or non primitive
+//Arays,Objects,Functions
+  
+//array//
+// const heros=["saktiman","nagaraj","doa"]
+
+// //objects
+
+// let myobj={
+//     name:"Happy",
+//     age:22,
+// }
+// //Function
+
+// const myfunction=function(){
+//     console.log("hello world")
+// }
+ 
+console.log(typeof anotherId)
