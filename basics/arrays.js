a0d9900451ec184c054688b3cd7a6f2cf1280e6c
rituraj677 <rajritu2107@gmail.com>
2026-02-28 07:33:34 +0000

    message

--- basics/arrays.js
+++ basics/arrays.js
@@ -1,29 +1,29 @@
 //array
 
-// const myarr=[0,1,2,3,4,5,]
-// myarr.push(6);
-// myarr.pop()
-//myarr.unshift(9)
-// myarr.shift() 
-// console.log(myarr)
+const myarr=[0,1,2,3,4,5,]
+myarr.push(6);
+myarr.pop()
+myarr.unshift(9)
+myarr.shift() 
+console.log(myarr)
 
 const marvel=["thor","ironman","spiderman"]
 const dc=["superman","flash","batman"]
 
-// marvel.push(dc);
-// console.log(marvel)
-// console.log(marvel[3][2])
+marvel.push(dc);
+console.log(marvel)
+console.log(marvel[3][2])
 
-// const all= marvel.concat(dc)
-//console.log(all)
+const all= marvel.concat(dc)
+console.log(all)
 
 const allnew=[...marvel,...dc]
 console.log(allnew)
 
 const another=[1,2,3,[4,5,6],[6,7,[4,5]]]
 
-// let usable_array=another.flat(Infinity)
-// console.log(usable_array)
+let usable_array=another.flat(Infinity)
+console.log(usable_array)
 
 console.log(Array.isArray("hitesh"))
 console.log(Array.from("happy"))
